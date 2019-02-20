ce9b39c1084c9aa1b61e89b99e24a00557299513
JUANMIGUELPAZ\migue <juanmpaz2@gmail.com>
2019-02-19 21:11:02 -0600

    Creacion de componentes
    Funcionando componentes

--- puxbit/src/app/app.module.ts
+++ puxbit/src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AppComponent } from './app.component';
 import { AlumnosComponent } from './componentes/alumnos/alumnos.component';
 import { ProfesoresComponent } from './componentes/profesores/profesores.component';
 import { CursosComponent } from './componentes/cursos/cursos.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { SidebarComponent } from './componentes/sidebar/sidebar.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlumnosComponent,
     ProfesoresComponent,
-    CursosComponent
+    CursosComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
